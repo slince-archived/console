6116d6975b655c76eb0c6ec07f1fe407be3082a3
slince <taosikai@yeah.net>
2016-01-26 17:43:54 +0800

    up

--- demo/index.php
+++ demo/index.php
@@ -1,33 +1,33 @@
 <?php
-use Slince\Console\Stdio;
 use Slince\Console\Command;
 use Slince\Console\Console;
-use Slince\Console\Argv;
+use Slince\Console\Context\Io;
+use Slince\Console\Context\Argv;
 
-// include __DIR__ . '/../vendor/autoload.php';
+include __DIR__ . '/../vendor/autoload.php';
 
-// class SayHalloCommand extends Command
-// {
-//     protected $name = 'hello';
+class SayHalloCommand extends Command
+{
+    protected $name = 'hello';
     
-//     function init()
-//     {
+    function init()
+    {
         
-//     }
+    }
     
-//     function execute(Stdio $io, Argv $argv)
-//     {
-//         $io->out("Say your name: ");
-//         $name = $io->in();
-//         $io->outln("Hello {$name}");
-//     }
-// }
+    function execute(Io $io, Argv $argv)
+    {
+        $io->out("Say your name: ");
+        $name = $io->in();
+        $io->outln("Hello {$name}");
+    }
+}
 
-// $console = new Console();
-// $console->addCommand(new SayHalloCommand());
-// $console->run();
+$console = new Console();
+$console->addCommand(new SayHalloCommand());
+$console->run();
 
-$options = 'ab';
-var_dump(getopt($options));
+// $options = 'ab';
+// var_dump(getopt($options));
 
 // var_dump($argv);
